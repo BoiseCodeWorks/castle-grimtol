62fb68bc1c3fba84248cf77f619503bc3dfe1011
Jake Overall <joverall22@gmail.com>
2017-06-15 11:10:27 -0600

    Create IGame.cs

--- Game/Interfaces/IGame.cs
+++ Game/Interfaces/IGame.cs
@@ -4,13 +4,10 @@ namespace CastleGrimtol.Game
 {
     public interface IGame
     {
-        int Score { get; set; }
-
         Room CurrentRoom { get; set; }
         Player CurrentPlayer { get; set; }
 
         void Setup();
-        void StartGame();
         void Reset();
 
         //No need to Pass a room since Items can only be used in the CurrentRoom
