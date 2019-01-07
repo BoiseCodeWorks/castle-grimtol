29589086266fbfb5a1bc05dc417f2180b95dee41
Porter Wilcox <40908011+porterwilcox@users.noreply.github.com>
2019-01-07 10:05:53 -0700

    List members are item interfaces not instances

--- Project/Interfaces/IPlayer.cs
+++ Project/Interfaces/IPlayer.cs
@@ -5,6 +5,6 @@ namespace CastleGrimtol.Project
   public interface IPlayer
   {
     string PlayerName { get; set; }
-    List<Item> Inventory { get; set; }
+    List<IItem> Inventory { get; set; }
   }
 }
